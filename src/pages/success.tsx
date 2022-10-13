66970bd02b448da2f99e70ea3669d6ce6aeddc5d
GabrielRCodes <gabrielrcodes@gmail.com>
2022-10-13 20:38:01 -0300

    Finalizando página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,59 @@
-export default function Success() {
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import Image from "next/future/image"
+
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
+
+      <p>
+        Olá <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho de sua casa! 🥳
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
+}
+
+/* Client-side fetch | UseEffect // Axios // getServerSideProps // getStaticProps */
+
+export const getServerSideProps: GetServerSideProps = async ({ query, params }) => {
+  
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"]
+  })
+
+  const customerName = session.customer_details.name;
+
+  const product = session.line_items.data[0].price.product as Stripe.Product
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      }
+    }
+  } 
 }
