cb144e3926b59b7860540a8155a64384fe970f91
GabrielRCodes <gabrielrcodes@gmail.com>
2022-10-13 20:43:07 -0300

    Adding fallback on success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -36,7 +36,16 @@ export default function Success({ customerName, product }: SuccessProps) {
 /* Client-side fetch | UseEffect // Axios // getServerSideProps // getStaticProps */
 
 export const getServerSideProps: GetServerSideProps = async ({ query, params }) => {
-  
+
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      }
+    }
+  }
+
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
